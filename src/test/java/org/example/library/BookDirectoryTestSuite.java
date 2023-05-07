03405791024b0a32c55f6f7a0a50466eb2ffd678
Kasia Gierasimczuk <k.gierasimczuk@gmail.com>
2023-05-07 14:53:04 +0200

    Additional logging in test again

--- src/test/java/org/example/library/BookDirectoryTestSuite.java
+++ src/test/java/org/example/library/BookDirectoryTestSuite.java
@@ -152,6 +152,7 @@ public class BookDirectoryTestSuite {
          borrowedBooks.add(book5);
          when(libraryDatabaseMock.listBooksInHandsOf(testUser)).thenReturn(borrowedBooks);
          System.out.println("test");
+         System.out.println("test23");
          //When
          List<Book> result = bookLibrary.listBooksInHandsOf(testUser);
 
